50095f095bbba3319120067772ee92d63f3a1333
Ian Bachiller <ianbachise@gmail.com>
2024-05-04 23:55:57 -0400

    completed inheritance lab

--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,23 @@ import random
 
 class Teacher(User):
 
+    knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+
+
     def teach(self):
-        pass
+        if len(self.knowledge) > 0 :
+            random_index = random.randint(0, len(self.knowledge) - 1)
+            return self.knowledge[random_index]
+        else:
+            return "No knowledge available" 
